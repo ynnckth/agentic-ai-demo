a1e0e6235703581f41e3690c967cea4d66953203
Yannick Streit <yannick.streit@zuehlke.com>
2026-02-07 22:45:29 +0800

    Update copilotkit dependencies

--- 07_agentic_ui/ui/src/App.tsx
+++ 07_agentic_ui/ui/src/App.tsx
@@ -6,12 +6,18 @@ function App() {
   return (
     <div className="app-container">
       <CopilotSidebar
+        defaultOpen={true}
+        hitEscapeToClose={false}
         disableSystemMessage={true}
         clickOutsideToClose={false}
         suggestions={[
           {
             title: 'Play a C major chord',
             message: 'Play a C major chord',
+          },
+          {
+            title: "Play 'Für Elise'",
+            message: 'Play the main melody of Beethoven’s "Für Elise"',
           },
         ]}
       >
